7dae4c7fd51df7d0e354bde066d5f30f23e8351a
MiguelAndrade866 <a31366@tomazpelayo.com>
2023-04-22 14:47:47 +0100

    exercicio11

--- PutcOlaMundo.c
+++ PutcOlaMundo.c
@@ -1,3 +1,15 @@
-// Resolva neste ficheiro o seu exercÃ­cio
-
+#include <stdio.h>
+int main(){
+    putc('O',stdout);
+    putc('l',stdout);
+    putc(' ',stdout);
+    putc(' ',stdout);
+    putc('M',stdout);
+    putc('u',stdout);
+    putc('n',stdout);
+    putc('d',stdout);
+    putc('o',stdout);
+    putc('!',stdout);
+    return(0);
+}
 
